ad715d605378f59569ea2b2ef634e0afef3d086c
Tetiana-KET <belangelphone@gmail.com>
2024-10-29 16:22:25 +0200

    feat: solve request.js

--- request.js
+++ request.js
@@ -22,9 +22,59 @@
  *      Like the first exercise but the urls must be crawled in parallel
  * --------------------------------------------------------------------------------------------------------------
  */
+import { got } from 'got-scraping';
+import * as cheerio from 'cheerio';
+import { parsePriceNumFirst } from './utils/parsePrice.js';
 
 const urls = [
     'https://www.miinto.it/p-de-ver-s-abito-slip-3059591a-7c04-405c-8015-0936fc8ff9dd',
     'https://www.miinto.it/p-abito-a-spalline-d-jeny-fdac3d17-f571-4b55-8780-97dddf80ef35',
     'https://www.miinto.it/p-abito-bianco-con-stampa-grafica-e-scollo-a-v-profondo-2b03a3d9-fab1-492f-8efa-9151d3322ae7'
 ];
+
+async function crawlUrls(url) {
+    try {
+        const response = await got(url)
+        if (response.statusCode !== 200) {
+            throw new Error(`Error: Status code ${response.statusCode} for URL ${url}`);
+        }
+        const $ = cheerio.load(response.body)
+
+        const discountedPriceText  = $('p[data-testid="product-previous-price"]').text().trim();
+        const fullPriceText = $('p[data-testid="product-price"]').text().trim();
+        
+        
+        const fullPrice = fullPriceText
+            ? parsePriceNumFirst(fullPriceText) : null;
+        const discountedPrice  = discountedPriceText ? parsePriceNumFirst(discountedPriceText) : fullPrice;
+        const currency = fullPrice ? fullPriceText.split(' ').pop() : 'N/A';
+        const title = $('h1[data-testid="product-name"]').text().trim();
+
+        const result = {
+            url,
+            fullPrice,
+            discountedPrice,
+            currency,
+            title,
+        }
+        console.log(result)
+       
+    } catch (error){
+        console.error(`Error scraping ${url}:`, error.message);
+    }
+}
+
+async function crawlInSequence(urls) {
+    console.log('This logs from crawl In Sequence')
+    for (const url of urls) {
+        await crawlUrls(url);
+    }
+}
+await crawlInSequence(urls);
+
+async function crawlInParallel(urls) {
+    console.log('This logs from crawl In Parallel')
+    await Promise.all(urls.map(url => crawlUrls(url)));
+}
+
+await crawlInParallel(urls);
--- utils/parsePrice.js
+++ utils/parsePrice.js
@@ -1,3 +1,7 @@
 export function parsePrice(price) {
 	return parseFloat(price.split(' ')[1].replace(',', '.').trim());
 }
+
+export function parsePriceNumFirst(price) {
+	return parseFloat(price.split(' ')[0].replace(',', '.').trim());
+}
